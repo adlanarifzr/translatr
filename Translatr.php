b056dd0c83664240366d4225805456c53ba99c76
Adlan Arif Zakaria <30769424+adlanarifzr@users.noreply.github.com>
2018-01-06 22:36:51 +0800

    Uploaded Trait file

---
+++ Translatr.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Traits;
+
+trait Translatr
+{
+    /**
+     * Get an attribute from the model.
+     *
+     * @param  string  $key
+     * @return mixed
+     */
+    public function getAttribute($key)
+    {
+        if (! $key) {
+            return;
+        }
+
+        // If the attribute exists in the attribute array or has a "get" mutator we will
+        // get the attribute's value. Otherwise, we will proceed as if the developers
+        // are asking for a relationship's value. This covers both types of values.
+        if (array_key_exists($key, $this->attributes) ||
+            $this->hasGetMutator($key)) {
+            return $this->getAttributeValue($key);
+        }
+
+        if (array_key_exists($key.'_'.app()->getLocale(), $this->attributes) ||
+            $this->hasGetMutator($key)) {
+            return $this->getAttributeValue($key.'_'.app()->getLocale());
+        }
+
+        // Here we will determine if the model base class itself contains this given key
+        // since we don't want to treat any of those methods as relationships because
+        // they are all intended as helper methods and none of these are relations.
+        if (method_exists(self::class, $key)) {
+            return;
+        }
+
+        return $this->getRelationValue($key);
+    }
+}
